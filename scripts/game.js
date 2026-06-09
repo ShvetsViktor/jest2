cf54018f5633dd935cb5613bebd60c0966e51ed0
Viktor <shvetsviktor89@gmail.com>
2026-06-09 16:11:45 +0100

    create tests for lightsOn() and the function itself

--- scripts/game.js
+++ scripts/game.js
@@ -5,6 +5,7 @@ let game = {
     choices: ['button1', 'button2', 'button3', 'button4'],
 }
 
+
 function newGame() {
     game.score = 0;
     game.currentGame = [];
@@ -13,10 +14,17 @@ function newGame() {
     addTurn();
 }
 /**
- * Should display 0 for the element with id score
- */
+ * Should display the value of game.score in the element with id score
+*/
 function showScore() {
     document.getElementById("score").innerText = game.score;
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light")
+    }, 400)
 }
 
 
@@ -26,11 +34,19 @@ function showScore() {
  * 2. Randomly add a button to the game.currentGame array
  * 3. Call showTurns() function  
  * 
- */
+*/
 function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    // showTurns();
+    showTurns();
 }
 
-module.exports = { game, newGame, showScore, addTurn };
+/**
+ * The showTurns() function and player clicks should cause the circle to change
+ * colour or to light up.   
+ */
+function showTurns() {
+
+}
+
+module.exports = { game, newGame, showScore, addTurn, showTurns, lightsOn };
